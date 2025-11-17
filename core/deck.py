45bc5ac3300f7a56e236094d5dd305fe890a7d61
hananya <chruderman@gmail.com>
2025-11-17 19:46:28 +0200

    added shuffel

--- core/deck.py
+++ core/deck.py
@@ -1,13 +1,52 @@
+import random
+
 def build_standard_deck() -> list[dict]:
+    """
+    building a matrix of cards
+    """
+    deck = []
     suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
     ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
     deck = [ {'suit' : suit, 'rank' : rank} for rank in ranks for suit in suits]
-    return(deck)
+    return deck
     
 
 
 def shuffle_by_suit(deck: list[dict], swaps: int = 5000) -> list[dict]:
+    
+    while swaps > 0:
+        idx_card_i = deck[random.randint(0,len(deck)-1)]
+        idx_card_i = deck.index(idx_card_i)
+        good_card = False
+        while not good_card:
+            idx_card_j = deck[random.randint(0,len(deck)-1)]
+            idx_card_j = deck.index(idx_card_j)
+            suit = deck[idx_card_i]['suit']
+            match suit:
+                case 'Hearts':
+                    if idx_card_j % 5 != 0:
+                        continue
+                case 'Clubs':
+                    if idx_card_j % 3 != 0:
+                        continue
+                case 'Diamonds':
+                    if idx_card_j % 2 != 0:
+                        continue
+                case 'Spades':
+                    if idx_card_j % 7 != 0:
+                        continue
+            good_card = True
+        deck[idx_card_i], deck[idx_card_j] = deck[idx_card_j], deck[idx_card_i]
+        swaps -= 1
+
+
+
     return deck
 
 if __name__ == "__main__":
     print(build_standard_deck())
+    deck = build_standard_deck()
+    # my_deck = build_standard_deck()
+    # print(build_standard_deck()[0]['suit'])
+    # print(my_deck[0]['rank'])
+    print(shuffle_by_suit(deck))
--- core/game_logic.py
+++ core/game_logic.py
@@ -1,8 +1,9 @@
-from core.deck import build_standard_deck, shuffle_by_suit
+from deck import build_standard_deck, shuffle_by_suit
+from player_io import ask_player_action
 
 def calculate_hand_value(hand: list[dict]) -> int: 
     sum = 0
-    for i in hand:
+    for i in hand["hand"]:
         if i['rank'] == 'A':
             sum +=1
         elif i['rank'] == 'J' or  i['rank'] == 'Q' or  i['rank'] == 'K':
@@ -15,34 +16,53 @@ def calculate_hand_value(hand: list[dict]) -> int:
 def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
     player["hand"].append(deck.pop())
     dealer["hand"].append(deck.pop())
-    return 
+    player["hand"].append(deck.pop())
+    dealer["hand"].append(deck.pop())
+
+     
 
 def dealer_play(deck: list[dict], dealer: dict) -> bool:
-    sum = calculate_hand_value(dealer["hand"])
+    """
+    the dealer pley till he has sum of 17
+    """
+    sum = 0
     while sum <= 17:
-        new_card = dealer["hand"].append(deck.pop())
-        sum += new_card['rank']
+        getting_a_new_card = dealer["hand"].append(deck.pop())
+        sum += calculate_hand_value(getting_a_new_card)
+    if 17 < sum <= 21:
+        return True
+    else:
+        return False
         
-    return sum
-
-
-
-
-
-def run_full_game(deck: list[dict], player: dict, dealer: dict) -> None: 
-    pass
-
-if __name__ == "__main__":
+    
+def run_full_game(player: dict, dealer: dict) -> None: 
     deck = build_standard_deck()
+    shufflled_deck = shuffle_by_suit(deck)
+    deal_two_each(shufflled_deck, player, dealer)
+    while calculate_hand_value(player) < 100000:
+        print("you drow the", str(player["hand"]))
+        if ask_player_action() == 'S':
+            break
+        else:
+            player["hand"].append(deck.pop())
+    print(player)
+             
 
-    print(deck)
 
-    shuffle = shuffle_by_suit(deck)
-    player = {"hand": [ ] } 
-    dealer = {"hand": [ ] }
+
+
+    # print(deck)
+    # print(len(deck))
+
+    # shuffle = shuffle_by_suit(deck)
+player = {"hand": [ ] } 
+dealer = {"hand": [ ] }
    
-    print(calculate_hand_value([{'suit' : 'Diamonds' , 'rank' : 'K'}, {'suit' : 'Hearts' , 'rank' : 'K'},]))
-    print(deal_two_each(deck, player, dealer))
-    print(dealer_play(deck, dealer))
-
+    # #print(calculate_hand_value([{'suit' : 'Diamonds' , 'rank' : 'K'}, {'suit' : 'Hearts' , 'rank' : 'K'},]))
+    #print(deal_two_each(deck, player, dealer))
+    # print(deck)
+    # print(len(deck))
+    # print(dealer_play(deck, dealer))
+    #print(deal_two_each(deck, player, dealer ))
+run_full_game(player, dealer)
     # py -m core.game_logic
